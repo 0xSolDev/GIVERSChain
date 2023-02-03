aa97ec0731f13697c66b552206e00953cc87edbf
0xSolDev <Musyaroful.Anas29@gmail.com>
2023-02-04 02:51:13 +1000

    original

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,5 @@
+
+
 require("dotenv").config();
 
 require("@nomiclabs/hardhat-etherscan");
@@ -14,7 +16,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -25,88 +27,101 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.5",
   networks: {
-    hardhat: {
+    hardhat:{
       forking: {
-        url: "https://bsc-dataseed.binance.org/",
-        chainId: 56,
+        url: process.env.ROPSTEN_URL,
         allowUnlimitedContractSize: true,
-        timeout: 90000,
-        blockNumber: 25263094,
-      },
+        timeout:90000,
+        //blockNumber:12325509
+      }
+      
     },
 
-    // ropsten: {
-    //   url: process.env.ROPSTEN_URL,
-    //   accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [process.env.PRIVATE_KEY0,process.env.PRIVATE_KEY1,process.env.PRIVATE_KEY2,process.env.PRIVATE_KEY3,process.env.PRIVATE_KEY4,process.env.PRIVATE_KEY5],
-    //   gas:5603244
+    ropsten: {
+      url: process.env.ROPSTEN_URL,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [process.env.PRIVATE_KEY0,process.env.PRIVATE_KEY1,process.env.PRIVATE_KEY2,process.env.PRIVATE_KEY3,process.env.PRIVATE_KEY4,process.env.PRIVATE_KEY5],
+      gas:5603244
 
-    // },
+    },
     bsctest: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
-      // gasPrice: 20000000000,
-      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 20000000000,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: "",
+      },
     },
 
-    //  roburna : {
-    //     url: process.env.ROBURNA_URL || 'https://preseed-testnet-1.roburna.com/',
-    //     accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [process.env.PRIVATE_KEY0,process.env.PRIVATE_KEY1,process.env.PRIVATE_KEY2,process.env.PRIVATE_KEY3,process.env.PRIVATE_KEY4,process.env.PRIVATE_KEY5],
-    //     gas:5603244,
-    //     chainId:159
+   roburna : {
+      url: process.env.ROBURNA_URL || 'https://preseed-testnet-1.roburna.com/',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [process.env.PRIVATE_KEY0,process.env.PRIVATE_KEY1,process.env.PRIVATE_KEY2,process.env.PRIVATE_KEY3,process.env.PRIVATE_KEY4,process.env.PRIVATE_KEY5],
+      gas:5603244,
+      chainId:159
 
-    //   },
+    },
+
+
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
   },
   etherscan: {
-    apiKey: "J53CKAMG8MUT2TZ23UEMDIEP2HADJ3PXFD",
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
+  
+    solidity: {
+      compilers: [
 
-  solidity: {
-    compilers: [
-      {
-        version: "0.4.18",
-        settings: {},
-      },
-      {
-        version: "0.4.20",
-      },
-
-      {
-        version: "0.4.24",
-      },
-      {
-        version: "0.8.5",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
+        {
+          version: "0.4.18",
+          settings: {},
         },
-      },
-
-      {
-        version: "0.8.7",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
+        {
+          version: "0.4.20",
         },
-      },
-
-      {
-        version: "0.8.14",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
+        
+        {
+          version: "0.4.24",
+  
         },
-      },
-    ],
+        {
+          version: "0.8.5",
+          settings: {
+            optimizer: {
+              enabled: true,
+              runs: 200
+            }
+          }
+        },
+
+        {
+          version: "0.8.7",
+          settings: {
+            optimizer: {
+              enabled: true,
+              runs: 200
+            }
+          }
+        },
+
+        {
+          version: "0.8.14",
+          settings: {
+            optimizer: {
+              enabled: true,
+              runs: 200
+            }
+          }
+        }
+        
+        
+      ],
+    
   },
   // mocha: {
   //   reporter: 'xunit',
